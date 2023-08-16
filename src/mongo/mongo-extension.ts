37b983514303e3fbace911d0e3b0e41a289f4f49
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2023-08-16 14:10:23 -0400

    Add documentation
    Closes #13

--- src/mongo/mongo-extension.ts
+++ src/mongo/mongo-extension.ts
@@ -1,12 +1,21 @@
-import { KiwiPreconditions } from "@kiwiproject/kiwi-js";
-import { GenericContainer } from "testcontainers";
+import {KiwiPreconditions} from "@kiwiproject/kiwi-js";
+import {GenericContainer} from "testcontainers";
 
+/**
+ * Starts a Mongo container and stores the container information in global.MONGO_CONTAINER.
+ *
+ * @param image The image name/version to use for mongo. Defaults to mongo:6.
+ */
 async function startMongoContainer(image: string = "mongo:6") {
   global.MONGO_CONTAINER = await new GenericContainer(image)
     .withExposedPorts(27017)
     .start();
 }
 
+/**
+ * Stops a previously started Mongo container. Error will be thrown if startMongoContainer is not
+ * previously called.
+ */
 async function stopMongoContainer() {
   KiwiPreconditions.checkState(
     global.MONGO_CONTAINER !== undefined,
@@ -16,6 +25,10 @@ async function stopMongoContainer() {
   global.MONGO_CONTAINER = undefined;
 }
 
+/**
+ * Retrieves the base connection URL for accessing the running Mongo. Error will be thrown if startMongoContainer
+ * is not previously called.
+ */
 function getMongoBaseUrl(): string {
   KiwiPreconditions.checkState(
     global.MONGO_CONTAINER !== undefined,
@@ -27,6 +40,11 @@ function getMongoBaseUrl(): string {
   return `mongodb://${host}:${port}/`;
 }
 
+/**
+ * Retrieves the base connection URL plus the given db name for accessing the running Mongo. Error will be thrown if
+ * startMongoContainer is not previously called.
+ * @param dbName The name of the database in mongo to connect.
+ */
 function getMongoUriWithDb(dbName: string): string {
   return `${getMongoBaseUrl()}${dbName}`;
 }
