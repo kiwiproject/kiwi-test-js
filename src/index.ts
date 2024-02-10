9447ec58a89b59bfbb99fb6eebd35c145957f5fd
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2024-02-10 08:19:01 -0500

    Forgot to add the redis extension to the index
    Bump version to 0.4.1

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { MongoExtension } from "./mongo/mongo-extension";
 export { PostgresExtension } from "./postgres/postgres-extension";
 export { ElasticSearchExtension } from "./elasticsearch/elastic-search-extension";
 export { MinioExtension } from "./minio/minio-extension";
+export { RedisExtension } from "./redis/redis-extension";
 export { wait } from "./waitForIt/waitForIt";
