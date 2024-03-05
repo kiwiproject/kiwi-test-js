52b51a4a59f9a732f5502bf7b0e31990a102a530
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2024-03-05 14:46:47 -0500

    Add enhancements for waitForIt
    * Add poll delay to delay the start of the check defaulting to no delay
    * Add an optional alias to be used when verbose logging is enabled
    * Add an option to enable verbose logging
    *Default units on all chain methods to milliseconds
    * Add js docs
    * Update tests
    * Bump to version 0.5.0

--- __tests__/waitForIt/waitForIt.t.ts
+++ __tests__/waitForIt/waitForIt.t.ts
@@ -1,16 +1,20 @@
-import { describe, expect, it } from "@jest/globals";
+import { afterEach, describe, expect, it, jest } from "@jest/globals";
 import { wait } from "../../src";
 
-describe("Awaitility", () => {
+describe("WaitForIt", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it("should resolve when condition passes", () => {
     wait().until(() => true);
   });
 
   it("should reject when condition runs out of tries", async () => {
     await expect(wait().until(() => false)).rejects.toEqual(
-      new Error("Condition was not met after 5 tries"),
+      new Error("Condition was not met after 40 tries"),
     );
-  });
+  }, 6_000);
 
   it("should allow for setting the wait time", async () => {
     await expect(
@@ -20,12 +24,107 @@ describe("Awaitility", () => {
     ).rejects.toEqual(new Error("Condition was not met after 2 tries"));
   });
 
+  it("should allow for setting the wait time defaulting units to ms", async () => {
+    await expect(
+      wait()
+        .atMost(200)
+        .until(() => false),
+    ).rejects.toEqual(new Error("Condition was not met after 2 tries"));
+  });
+
   it("should allow for setting the interval check time", async () => {
     await expect(
       wait()
         .atMost(200, "ms")
-        .checkEvery(10, "ms")
+        .withPollInterval(10, "ms")
         .until(() => false),
     ).rejects.toEqual(new Error("Condition was not met after 20 tries"));
+  });
+
+  it("should allow for setting the interval check time defaulting to ms units", async () => {
+    await expect(
+      wait()
+        .atMost(200, "ms")
+        .withPollInterval(10)
+        .until(() => false),
+    ).rejects.toEqual(new Error("Condition was not met after 20 tries"));
+  });
+
+  it("should allow for setting the poll delay time", async () => {
+    await expect(
+      wait()
+        .atMost(200, "ms")
+        .withPollInterval(10, "ms")
+        .withPollDelay(20, "ms")
+        .until(() => false),
+    ).rejects.toEqual(new Error("Condition was not met after 20 tries"));
+  });
+
+  it("should allow for setting the poll delay time defaulting the units to ms", async () => {
+    await expect(
+      wait()
+        .atMost(200, "ms")
+        .withPollInterval(10, "ms")
+        .withPollDelay(20)
+        .until(() => false),
+    ).rejects.toEqual(new Error("Condition was not met after 20 tries"));
+  });
+
+  it("should allow for enabling verbose mode with success messages", async () => {
+    const consoleSpy = jest.spyOn(global.console, "log");
+
+    await expect(
+      wait()
+        .atMost(200, "ms")
+        .withPollInterval(10, "ms")
+        .withVerbose()
+        .until(() => true),
+    ).resolves.toEqual("Condition met after 1 of 20 tries");
+
+    expect(consoleSpy).toHaveBeenCalledWith(
+      "Waiting for condition for up to 20 attempts checking every 10 ms",
+    );
+    expect(consoleSpy).toHaveBeenCalledWith(
+      "Condition met after 1 of 20 tries",
+    );
+  });
+
+  it("should allow for enabling verbose mode with failed messages", async () => {
+    const consoleSpy = jest.spyOn(global.console, "log");
+
+    await expect(
+      wait()
+        .atMost(200, "ms")
+        .withPollInterval(10, "ms")
+        .withVerbose()
+        .until(() => false),
+    ).rejects.toEqual(new Error("Condition was not met after 20 tries"));
+
+    expect(consoleSpy).toHaveBeenCalledWith(
+      "Waiting for condition for up to 20 attempts checking every 10 ms",
+    );
+    expect(consoleSpy).toHaveBeenCalledWith(
+      "Condition was not met after 20 tries",
+    );
+  });
+
+  it("should allow for adding an alias ", async () => {
+    const consoleSpy = jest.spyOn(global.console, "log");
+
+    await expect(
+      wait()
+        .atMost(200, "ms")
+        .withPollInterval(10, "ms")
+        .withVerbose()
+        .withAlias("KIWI")
+        .until(() => true),
+    ).resolves.toEqual("Condition with alias KIWI met after 1 of 20 tries");
+
+    expect(consoleSpy).toHaveBeenCalledWith(
+      "Waiting for condition with alias KIWI for up to 20 attempts checking every 10 ms",
+    );
+    expect(consoleSpy).toHaveBeenCalledWith(
+      "Condition with alias KIWI met after 1 of 20 tries",
+    );
   });
 });
